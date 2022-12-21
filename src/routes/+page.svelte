1b71c592827d3afe07165a3f3118bf8120a276f2
grizhlieCodes <rafal.potasz@gmail.com>
2022-12-21 11:02:10 +0000

    removed unused imports

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
 	import Header from '$lib/components/Header.svelte';
 	import Card from '$lib/ui/Card.svelte';
-	import selectedStore from '$lib/stores/selected';
 	import TopSelections from '$lib/components/TopSelections.svelte';
 	import Form from '$lib/components/Form.svelte';
 	import Clamps from '$lib/components/Clamps.svelte';
 	import { onMount } from 'svelte';
 	import clamps from '$lib/stores/clamps';
 	import darkmode from '$lib/stores/darkmode'
-	let mounted = false;
 
 	onMount(() => {
 		clamps.updateClampsFromLocalData();
@@ -16,14 +14,14 @@
 	});
 </script>
 
-<main class="min-w-screen min-h-[150vh] w-full bg-slate-50 dark:bg-[#0A1423]
+<main class="min-w-screen h-max pb-40 w-full bg-slate-50 dark:bg-[#0A1423]
 transition-colors ">
 	<Header />
 	<div
 		class="flex h-max min-h-[30rem] w-full flex-wrap items-start justify-center gap-20 py-20
-	px-6"
+	px-6 "
 	>
-		<Card>
+		<Card styling="">
 			<TopSelections />
 			<Form />
 		</Card>
